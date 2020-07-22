6a0f022ac31410de15d35a7c03f854a8f2a6e28b
Ashleigh <ashleigh0017@gmail.com>
2020-07-22 11:29:47 +0200

    Wip: create interface to accept an input - requirement for robot movements later

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 console.log('Hello World');
+
+const readline = require('readline')
+const input = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+input.question("How many bots would you like to simulate ? ", function (numberOfBots) {
+    input.question("Please specify the bots Coordinates ? ", function (coordinates){
+       console.log(`${numberOfBots} robot will move to ${coordinates}`)
+        input.close();
+    });
+});
