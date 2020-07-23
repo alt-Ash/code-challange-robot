abbc820110b5874e4ad9d678792522483b9e809a
Ashleigh <ashleigh0017@gmail.com>
2020-07-23 08:49:16 +0200

    WIP: Created Movement -  Started work on the class to make the robots move.

---
+++ movements.js
@@ -0,0 +1,7 @@
+/**
+ * North movements require position changes by increasing the y axis coordinate
+ * South movements requires position change by decreasing the y axis coordinate
+ * East movements requires position change by increasing the x axis coordinate
+ * West movements requires position change by decreasing the x axis coordinate
+ */
+
