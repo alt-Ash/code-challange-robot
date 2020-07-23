93a5d7864a106e49077fb41f5ae116cdffecebd0
Ashleigh <ashleigh0017@gmail.com>
2020-07-23 08:53:52 +0200

    WIP: Created Movement -  created move logic function.

--- movements.js
+++ movements.js
@@ -5,3 +5,18 @@
  * West movements requires position change by decreasing the x axis coordinate
  */
 
+function move(position) {
+    if (position.orientation === 'N') {
+        return {...position, "y": position.y + 1};
+    }
+    if (position.orientation === 'E') {
+        return {...position, "x": position.x + 1};
+    }
+    if (position.orientation === 'S') {
+        return {...position, "y": position.y - 1};
+    }
+    if (position.orientation === 'W') {
+        return {...position, "x": position.x - 1};
+    }
+    return position;
+}
